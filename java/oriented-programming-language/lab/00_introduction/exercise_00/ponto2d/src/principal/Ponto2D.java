c35bac7de1d825b8efd39a5643b057df32fbd96c
Thiago Gabriel <thiagogabriel.tgso@gmail.com>
2026-03-19 11:55:10 -0300

    first code exercise zero java

--- java/oriented-programming-language/lab/00_introduction/exercise_00/ponto2d/src/principal/Ponto2D.java
+++ java/oriented-programming-language/lab/00_introduction/exercise_00/ponto2d/src/principal/Ponto2D.java
@@ -2,4 +2,24 @@ package principal;
 
 public class Ponto2D {
 
+    private double x;
+    private double y;
+    
+    public Ponto2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public double distancia(Ponto2D outro) {
+        double dx = this.x - outro.x;
+        double dy = this.y - outro.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
