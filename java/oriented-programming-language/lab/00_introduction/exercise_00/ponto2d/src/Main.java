edadf8db94e45939d03a47f4e7ac22de8427b4a2
Thiago Gabriel <thiagogabriel.tgso@gmail.com>
2026-03-19 13:55:57 -0300

    Finishing exercise 0 java lab

---
+++ java/oriented-programming-language/lab/00_introduction/exercise_00/ponto2d/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ponto2D p1 = new Ponto2D(0, 0);
+        Ponto2D p2 = new Ponto2D(3, 4);
+        System.out.println("Distância entre os pontos: " + p1.distancia(p2));
+    }
+
+}
